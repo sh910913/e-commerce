b58cba49d08ddba17d736d4afe078d60270e7a6b
gordon.liao <sh910913@gmai.com>
2022-06-19 07:14:53 +0800

    fix error code name.

--- merchants/src/main/java/merchants/constant/ErrorCode.java
+++ merchants/src/main/java/merchants/constant/ErrorCode.java
@@ -7,7 +7,7 @@ public enum ErrorCode {
     MERCHANT_BUSINESS_LICENSE(3,"Merchant's business license is empty"),
     MERCHANT_PHONE_NUMBER_IS_INCORRECT(4,"Merchant's phone number is incorrect"),
     MERCHANT_ADDRESS_IS_EMPTY(5,"Merchant's address is empty"),
-    MERCHANT_NUMBER_DOES_NOT_EXIST(6,"Merchant's does not exist");
+    MERCHANT_NUMBER_DOES_EXIST(6,"Merchant's does exist");
     private Integer code;
     private String desc;
 
